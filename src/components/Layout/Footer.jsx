bb5c34b3864f30903df160aeba06c2b6b7023055
Paula <paulandreasl40@gmail.com>
2023-10-15 21:33:51 -0500

    fix footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="fixed bottom-0 w-full bg-violet-500 text-white p-4">
+    <div className="w-full bg-violet-500 text-white p-4 mt-11">
       <h4 className="text-center">Contenido del Footer</h4>
     </div>
   );
